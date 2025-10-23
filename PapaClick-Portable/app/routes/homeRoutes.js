b1c2c25a16797a9e6fffe2b6f8f3d05250a865ea
IosmilROjas <75073339>
2025-10-23 09:36:30 -0500

    23/10/25

--- PapaClick-Portable/app/routes/homeRoutes.js
+++
@@ -1,9 +0,0 @@
-// Rutas para la página de inicio
-const express = require('express');
-const router = express.Router();
-const HomeController = require('../control/homeController');
-
-// Ruta principal - página de inicio
-router.get('/', HomeController.mostrarInicio);
-
-module.exports = router;
